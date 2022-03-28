3d8cce698e4a4618d63fdaeea5a7e0c48bc40371
a.n.darashevich <a.n.darashevich@gmail.com>
2022-03-28 12:09:27 +0300

    Updates

---
+++ precache-manifest.31c8e48ef0f889a59d1ad766496c4ef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4155360d1d129bbea4ccb62a4ff006f",
+    "url": "/IgnatHomework/index.html"
+  },
+  {
+    "revision": "0fc3a8b4df54fe6923b0",
+    "url": "/IgnatHomework/static/css/main.ef93e424.chunk.css"
+  },
+  {
+    "revision": "e0b7144f82fe0e0bb115",
+    "url": "/IgnatHomework/static/js/2.ff528849.chunk.js"
+  },
+  {
+    "revision": "7c3782a7e65f48d3b45b3ebb61610f23",
+    "url": "/IgnatHomework/static/js/2.ff528849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc3a8b4df54fe6923b0",
+    "url": "/IgnatHomework/static/js/main.8b882f2e.chunk.js"
+  },
+  {
+    "revision": "3565b09d55eedcbd8020",
+    "url": "/IgnatHomework/static/js/runtime-main.fc562a4f.js"
+  }
+]);
