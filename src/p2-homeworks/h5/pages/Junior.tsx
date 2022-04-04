6d137f539f865b468616ead68d9f46b10cf81dea
a.n.darashevich <a.n.darashevich@gmail.com>
2022-04-04 12:48:24 +0300

    HW6

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -3,11 +3,7 @@ import React from 'react'
 function Junior() {
     return (
         <div>
-            {/*<HW1/>*/}
-            {/*<HW2/>*/}
-            {/*<HW3/>*/}
-            {/*<HW4/>*/}
-            {/*<HW6/>*/}
+            Junior
 
         </div>
     )
--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -3,11 +3,7 @@ import React from 'react'
 function JuniorPlus() {
     return (
         <div>
-            {/*<HW1/>*/}
-            {/*<HW2/>*/}
-            {/*<HW3/>*/}
-            {/*<HW4/>*/}
-            {/*<HW6/>*/}
+            JuniorPlus
 
         </div>
     )
